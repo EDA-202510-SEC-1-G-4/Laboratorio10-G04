1ad8a2fb4bc1acf107fc905462a5f1014b82d7a5
JD061905 <jd.echeverry@uniandes.edu.co>
2025-05-02 06:53:39 -0500

    Tome daniel

--- DataStructures/Priority_queue/priority_queue.py
+++ DataStructures/Priority_queue/priority_queue.py
@@ -1,9 +1,12 @@
 from DataStructures.List import array_list as lt
-from DataStructures.Map import 
+from DataStructures.Map import
 from DataStructures.Map import 
 from DataStructures.Map import 
 
-def new_heap():
+def new_heap(is_min_pq=True):
+    heap = {'elements':al.new_list(),
+            'size':0,
+            'cmp_function':is_min_pq}
     return
 
 def size():
@@ -21,8 +24,14 @@ def insert():
 def remove():
     return
 
-def swim():
+def swim(heap,pos):
     return 
 
 def sink():
     return 
+
+def default_compare_higher_value():
+    return
+
+def default_compare_lower_value():
+    return
