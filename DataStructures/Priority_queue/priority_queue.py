2d32bd16e4c703cb4fe3f57d83601267f1034da9
JD061905 <jd.echeverry@uniandes.edu.co>
2025-05-02 06:46:27 -0500

    La mente maestra

--- DataStructures/Priority_queue/priority_queue.py
+++ DataStructures/Priority_queue/priority_queue.py
@@ -1,3 +1,28 @@
 from DataStructures.List import array_list as lt
+from DataStructures.Map import 
+from DataStructures.Map import 
+from DataStructures.Map import 
 
+def new_heap():
+    return
 
+def size():
+    return
+
+def is_empty():
+    return 
+
+def get_first_priority():
+    return 
+
+def insert(): 
+    return 
+
+def remove():
+    return
+
+def swim():
+    return 
+
+def sink():
+    return 
