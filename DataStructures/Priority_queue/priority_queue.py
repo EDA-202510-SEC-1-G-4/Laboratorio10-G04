f3ea5a56fa2b910680c530687d610fa5b519eadb
DanRin9 <da.rinconr12@uniandes.edu.co>
2025-05-05 14:08:14 -0500

    sink puede ser pa

--- DataStructures/Priority_queue/priority_queue.py
+++ DataStructures/Priority_queue/priority_queue.py
@@ -1,8 +1,6 @@
 from DataStructures.List import array_list as al
 from DataStructures.Priority_queue import index_pq_entry as pqe
-from DataStructures.Map import
-from DataStructures.Map import 
-from DataStructures.Map import 
+
 
 def new_heap(is_min_pq=True):
     heap = {'elements':al.new_list(),
@@ -58,50 +56,57 @@ def mayor_prioridad(heap,nodo1,nodo2):
     else:
         return nodo2
     
-def es_hoja(pos,heap):
-    if 2*pos > heap["size"] and (2*pos)+1 > heap["size"]:
+def tiene_hijos(pos,heap):
+    if 2*pos < heap["size"] and (2*pos)+1 < heap["size"]:
+        return True
+    return False
+    
+def tiene_hijo(pos,heap):
+    if 2*pos < heap["size"]:
         return True
     return False
 
-def tiene_hijo(pos,heap):
-    if 2*pos < heap["size"] or (2*pos)+1 < heap["size"]:
-        return True
-    return False
+def get_hijos(pos,heap):
+    hijo2 = heap["elements"]["elements"][pos]
+    hijo1 = heap["elements"]["elements"][pos]
+    
+    if tiene_hijo(pos,heap):    
+        hijo1 = heap["elements"]["elements"][(2*pos)]
+        if tiene_hijos(pos,heap):
+            hijo2 = heap["elements"]["elements"][(2*pos)+1]
+            
+    return hijo1,hijo2
 
 def sink(heap,pos):
     if not is_empty(heap):
         func_comp = heap["cmp_function"]
-        padre = heap["elements"]["elements"][(pos)]
-        if tiene_hijo(pos,heap):
-            if 2*pos < heap["size"]:
-                hijo1 = heap["elements"]["elements"][(2*pos)]
-                if (2*pos)+1 < heap["size"]:
-                    hijo2 = heap["elements"]["elements"][(2*pos)+1]
-
-            while func_comp(padre,hijo1) or func_comp(padre,hijo2) and tiene_hijo(pos,heap):
-                    hijo = mayor_prioridad(hijo1,hijo2)
-                    temp = padre
-                    padre = hijo
-                    hijo = temp
-                    if hijo == hijo1:
-                        pos *=2
-                    elif hijo == hijo2:
-                        pos = (2*pos) + 1
-                    padre = heap["elements"]["elements"][(pos)]
-                    hijo1 = heap["elements"]["elements"][(2*pos)]
-                    hijo2 = heap["elements"]["elements"][(2*pos)+1]
+        padre = heap["elements"]["elements"][pos]
+        hijo1,hijo2 = get_hijos(pos,heap)
+        while func_comp(padre,hijo1) or func_comp(padre,hijo2) and tiene_hijo(pos,heap):
+                hijo = mayor_prioridad(hijo1,hijo2)
+                temp = padre
+                padre = hijo
+                hijo = temp
+                if hijo == hijo1:
+                    pos *=2
+                elif hijo == hijo2:
+                    pos = (2*pos) + 1
+                padre = heap["elements"]["elements"][pos]
+                hijo1,hijo2=get_hijos(pos,heap)
+    
+    return heap
             
 
     
 
 
-def default_compare_higher_value(father_node, child_node,retorno=False):
+def default_compare_higher_value(father_node, child_node):
     if pqe.get_key(father_node) >= pqe.get_key(child_node):
         
         return True
     return False
 
-def default_compare_lower_value(father_node, child_node,retorno=False):
+def default_compare_lower_value(father_node, child_node):
     if pqe.get_key(father_node) <= pqe.get_key(child_node):
         return True
     return False
